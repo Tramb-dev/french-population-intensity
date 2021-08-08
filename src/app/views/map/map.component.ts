4892b516c0537cdc391b14673c75c588908dd5d1
Bertrand Ravier <bertrand.ravier@gmail.com>
2021-08-08 11:37:43 +0200

    intégration d'événements

--- src/app/views/map/map.component.ts
+++ src/app/views/map/map.component.ts
@@ -8,18 +8,24 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  geojson: any;
+  myMap: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    const myMap = L.map('mapId').setView([48.853, 2.35], 6);
+    // Initialisation de la carte
+    this.myMap = L.map('mapId').setView([48.853, 2.35], 6);
     L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
         maxZoom: 20,
         attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-      }).addTo(myMap);
+      }).addTo(this.myMap);
 
+    // Chargement du fichier Geojson pour l'intégration des départements
     this.http.get('assets/data/departements.geojson').subscribe((departements: any) => {
-      this.http.get('/population').subscribe((populations: any) => {
+      // Chargement de la densité de population
+      this.http.get('assets/data/population-dep.json').subscribe((populations: any) => {
+      //this.http.get('/population').subscribe((populations: any) => {
         for (const departement of departements.features) {
           for (const population of populations) {
             if (departement.properties.code === population.code) {
@@ -27,21 +33,60 @@ export class MapComponent implements OnInit {
             }
           }
         }
-        L.geoJSON(departements, { style: (feature: any) => {
-          return {
-            fillColor: this.getColor(feature.properties.density),
-            weight: 2,
-            opacity: 1,
-            color: 'white',
-            dashArray: '3',
-            fillOpacity: 0.7
-          };
-        } }).addTo(myMap);
+        this.geojson = L.geoJSON(departements, { 
+          style: (feature: any) => {
+            return {
+              fillColor: this.getColor(feature.properties.density),
+              weight: 2,
+              opacity: 1,
+              color: 'white',
+              dashArray: '3',
+              fillOpacity: 0.7
+            }
+          },
+          onEachFeature: (feature: any, layer: any) => {
+            layer.on('mouseover', this.highlightFeature);
+            layer.on('mouseout', (event: any) => {
+              this.resetHighlight(event, this.geojson);
+            });
+            layer.on('click', (event: any) => {
+              this.zoomToFeature(event, this.myMap);
+            });
+          },
+        }).addTo(this.myMap);
       });
     });
   }
 
-  getColor(density: number): string {
+  private highlightFeature(event: any): void {
+    const layerCible = event.target;
+
+    layerCible.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layerCible.bringToFront();
+    }
+  }
+
+  private resetHighlight(event: any, geojson: any): void {
+    geojson.resetStyle(event.target);
+  }
+
+  private zoomToFeature(event: any, myMap: any) {
+    myMap.fitBounds(event.target.getBounds());
+  }
+
+  /**
+   * Colorie les départements en fonction de leur densité
+   * @param density densité de la population
+   * @returns une couleur en fonction de la densité
+   */
+  private getColor(density: number): string {
     return  density > 2000000 ? '#800026' :
             density > 1500000  ? '#BD0026' :
             density > 1000000  ? '#E31A1C' :
